16a4e8bf48cc76826c7df23bfd7ef8390c07cf5a
Kevin Lu <kevinlu3234@gmail.com>
2022-08-11 21:06:34 -0700

    new

---
+++ Assets/Attack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator ZombieAnimations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ZombieAnimations=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter (Collider other){
+        Debug.Log("attack");
+        ZombieAnimations.SetBool("IsRunning", false);
+        ZombieAnimations.SetTrigger("DoAttack");
+        
+    }
+}
